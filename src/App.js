9aefa70cce9c6963ce97b47f8b21e6d2fac464b3
Kiran <kirandeepkaurfdk5@gmail.com>
2023-05-13 18:13:25 -0800

    new layout

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ function App() {
       <div className="box1">HEADER</div>
       <div className="box2">NAVIGATION MENU</div>
       <div className="box3">
-        <div className="left">CONTENT</div>
+        <div className="left">
+          CONTENT
+          <div className="top">Search </div>
+          <div className="bottom">Social Media</div>
+          <div className="middle">News Letter Signup</div>
+        </div>
         <div className="center">MAIN CONTANT</div>
-        <div className="right">CONTANT</div>
+        <div className="right">
+          CONTANT
+          <div className="first">
+            Home
+            <div className="divide">Side nav</div>
+          </div>
+          <div className="second">Other Offers</div>
+        </div>
       </div>
       <div className="box4">FOOTER</div>
     </div>
